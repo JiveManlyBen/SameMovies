6744953ebc4e2b97bd9e16e881115a60f86a1bf4
Ben Levy <contact@benlevy.com>
2014-01-26 12:14:06 -0600

    Added some controls for displaying search results

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
+app.use(express.bodyParser());
 app.use(express.cookieParser('your secret here'));
 app.use(express.session());
 app.use(app.router);
@@ -33,6 +34,8 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/about', routes.about);
 app.get('/movie', movie.list);
+app.get('/compare', movie.compareForm);
+app.post('/compare', movie.compare);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/movie.js
+++ routes/movie.js
@@ -6,3 +6,13 @@
 exports.list = function(req, res){
   res.send("respond with a resource");
 };
+
+exports.compareForm = function(req, res){
+  res.render('compare', { title: 'Compare movies', section: 'movies', action: 'compare', results: null });
+};
+
+exports.compare = function(req, res){
+  var results = { first: [{"Title":"MASH","Year":"1970","imdbID":"tt0066026","Type":"movie"},{"Title":"Mash Up","Year":"2011","imdbID":"tt1877697","Type":"movie"}],
+    second: [{"Title":"Star Trek","Year":"2009","imdbID":"tt0796366","Type":"movie"},{"Title":"Star Trek Into Darkness","Year":"2013","imdbID":"tt1408101","Type":"movie"}] };
+  res.render('compare', { title: 'Compare movies', section: 'movies', action: 'compare', results: results });
+};
