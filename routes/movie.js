7b7de50d05caa364ae9ad2544cc56f10181c62b3
Ben Levy <contact@benlevy.com>
2014-02-01 11:07:23 -0600

    Send requests to OMDb service to get list of movies

--- routes/movie.js
+++ routes/movie.js
@@ -12,7 +12,45 @@ exports.compareForm = function(req, res){
 };
 
 exports.compare = function(req, res){ 
+  function pathForSearch(term) {
+    return '/?i=&s=' + term.replace("  ", " ", "gi").replace(" ", "+", "gi");
+  }
+  function requestError(err) {
+    console.error(err+""); 
+    console.error("Could not connect to " + options.host);
+    res.render('compare', options.page);
+  }
   var terms = [ req.body.first, req.body.second ];
+  var options = {
+    host: 'www.omdbapi.com',
+    port: 80,
+    path: '/?i=&s=',
+    method: 'POST',
+    queries: [ req.body.first, req.body.second ],
+    page: { title: 'Compare Movies', section: 'movies', action: 'compare', results: [], terms: terms }
+  };
 
-  res.render('compare', { title: 'Compare movies', section: 'movies', action: 'compare', results: [], terms: terms });
+  options.path = pathForSearch(terms[0]);
+  var request1 = require('http').request(options, function(response1) {
+    response1.setEncoding('utf8');
+    response1.on('data', function(chunk1) {
+      console.log('body: ' + chunk1);
+      options.path = pathForSearch(terms[1]);
+      var result = JSON.parse(chunk1).Search;
+      if (result)
+        options.page.results.push(result);
+      var request2 = require('http').request(options, function(response2) {
+        response2.setEncoding('utf8');
+        response2.on('data', function(chunk2) {
+          console.log('body: ' + chunk2);
+          var result = JSON.parse(chunk2).Search;
+          if (result)
+            options.page.results.push(result);
+          res.render('compare', options.page);
+        });
+      });
+      request2.on('error', requestError).end();
+    });
+  });
+  request1.on('error', requestError).end();
 };
