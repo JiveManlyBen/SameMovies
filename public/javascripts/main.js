d326e51e9a8156093563c0b3bd5c145b218a17e3
Ben Levy <contact@benlevy.com>
2014-02-04 21:46:45 -0600

    Added code to allow switching selection using the dropdown

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,23 +2,35 @@ jQuery(function ($) {
   function readResultsFromComponents() {
     ["first", "second"].map(function(element, index) {
       if ($("button.compare-results."+element).length > 0) {
-        var result = new Media($("button.compare-results."+element).text(), $("button.compare-results."+element).val());
-        compareResults[index].push(result);
-        $("a.compare-results."+element).each(function(i, element) {
-          var result = getMediaFromListItem($(this));
-          compareResults[index].push(result);
+        $("a.compare-results."+element).each(function(i, e) {
+          var media = getMediaFromListItem($(this));
+          compareResults[media.imdbID] = media;
         });
       }
+    });
+  }
+  function updateSelectionOptions(key, selectedID) {
+    $("a.compare-results."+key).each(function(i, e) {
+      var media = getMediaFromListItem($(this));
+      if (media.imdbID === selectedID) {
+        $(this).hide();
+        $("button.compare-results."+key).val(media.imdbID);
+        $("button.compare-results."+key).text(media.Title);       
+        $("#"+key).val(media.Title);
+      }
+      else
+        $(this).show();
     });
   }
   function getMediaFromListItem(e) {
     return new Media(e.text(), e.attr("href").replace("#", ""));
   }
-  var compareResults = [[], []];
+  var compareResults = [];
 
   readResultsFromComponents();
   $(".dropdown-menu li a").click(function(event){
     console.log("imdbID: " + getMediaFromListItem($(this)).imdbID);
+    updateSelectionOptions($(this).hasClass("first") ? "first" : "second", getMediaFromListItem($(this)).imdbID);
     $(this).parents(".btn-group").removeClass("open");
     event.preventDefault();
     return false;
