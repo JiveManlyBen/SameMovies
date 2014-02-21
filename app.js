d87d66690c258090a6ee69313a58736eb83cf8b5
Ben Levy <contact@benlevy.com>
2014-02-20 19:58:25 -0600

    Added internationalization of to the navigation bar

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ var http = require('http');
 var path = require('path');
 var routes = require('./routes');
 var movie = require('./routes/movie');
+var i18n = require('i18n');
+i18n.configure({
+    locales:['en', 'es'],
+    directory: __dirname + '/locales'
+});
 
 var app = express();
 
@@ -25,6 +30,11 @@ app.use(express.cookieParser('your secret here'));
 app.use(express.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(i18n.init);
+app.locals({
+  '__':  i18n.__
+  ,'ln': i18n.__n	
+});
 
 // development only
 if ('development' == app.get('env')) {
@@ -40,3 +50,4 @@ app.post('/compare', movie.compare);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
